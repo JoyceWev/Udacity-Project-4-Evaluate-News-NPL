464796aff5ee400408bd36fbe0494fdec0b25591
JoyceWev <joycevanweverwijk@gmail.com>
2020-04-23 11:21:54 +0200

    -

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
+const json = require('./package.json');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	entry: './src/client/index.js',
@@ -9,8 +11,19 @@ module.exports = {
 				test: '/\.js$/',
 				exclude: /node_modules/,
 				loader: "babel-loader"
+			},
+			{
+				type: 'javascript/auto',
+				test: '/\.json$/',
+        		loader: 'json-loader'
 			}
 		]
-	}
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: "./src/client/views/index.html",
+			filename: "./index.html",
+		})
+	]
 	//output: { ...output options }
 }
