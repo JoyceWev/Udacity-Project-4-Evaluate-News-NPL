a355f185f5f6b8c367331ab7041169017184f71b
JoyceWev <joycevanweverwijk@gmail.com>
2020-04-23 17:11:47 +0200

    - lesson 4

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,8 @@ const path = require('path');
 const webpack = require('webpack');
 const json = require('./package.json');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
 	mode: 'production',
@@ -17,6 +19,10 @@ module.exports = {
 				type: 'javascript/auto',
 				test: '/\.json$/',
         		loader: 'json-loader'
+			},
+			{
+	        	test: /\.scss$/,
+	       		use: [ 'style-loader', 'css-loader', 'sass-loader' ]
 			}
 		]
 	},
@@ -24,7 +30,17 @@ module.exports = {
 		new HtmlWebPackPlugin({
 			template: "./src/client/views/index.html",
 			filename: "./index.html",
-		})
+		}),
+	        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+   		}),
+	         new BundleAnalyzerPlugin()
 	]
 	//output: { ...output options }
 }
