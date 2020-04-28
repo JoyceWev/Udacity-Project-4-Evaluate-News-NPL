6ebd365dd01b4dd3b601d2dd01b88d98c16f2d49
JoyceWev <joycevanweverwijk@gmail.com>
2020-04-28 14:27:03 +0200

    -

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,14 +1,16 @@
 export function handleSubmit(event) {
-    event.preventDefault()
+        let baseURL = 'http://api.openweathermap.org/data/2.5/forecast?zip=';
+        const apiKey = '&APPID=5a0022d0a3d01f08d8587c491a1cc4c1';
+        event.preventDefault()
+        // check what text was put into the form field
+        let formText = document.getElementById('name').value
+        Client.checkForName(formText)
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
-
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+        console.log("::: Form Submitted :::")
+        fetch(baseURL+formText+apiKey)
+        .then(res => res.json())
+        .then(function(res) {
+            document.getElementById('results').innerHTML = res.city.name;
+            console.log(res);
+        });
 }
