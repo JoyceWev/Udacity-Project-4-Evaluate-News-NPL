5196bfe5eaeff695b8b64993d1ffa1fbed33d340
JoyceWev <joycevanweverwijk@gmail.com>
2020-04-29 10:52:47 +0200

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,8 @@ const webpack = require('webpack');
 const json = require('./package.json');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 //const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
@@ -44,7 +46,8 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
    		}),
-	        //new BundleAnalyzerPlugin()
+	        //new BundleAnalyzerPlugin(),
+	        new WorkboxPlugin.GenerateSW()
 	]
 	//output: { ...output options }
 }
