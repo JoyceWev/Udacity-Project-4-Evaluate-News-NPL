679bf2a7b246a65cdf5457006532175ca07bfde3
JoyceWev <joycevanweverwijk@gmail.com>
2020-05-01 14:40:29 +0200

    -

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,23 +1,15 @@
 export function handleSubmit(event) {
+        let baseURL = 'http://api.openweathermap.org/data/2.5/forecast?zip=';
+        const apiKey = '&APPID=5a0022d0a3d01f08d8587c491a1cc4c1';
         event.preventDefault()
-        let formURL = document.getElementById('name').value
-        if(validURL(formURL) === true){
-        	console.log("::: Data posted :::")
-	        // check what text was put into the form field
-	        console.log("::: Form Submitted :::")
-	        Client.postData('/sentiment', {URL: formURL})
-        } else{
-        	alert("The URL you entered is not a valid URL. Please try again.")
-        }
+        // check what text was put into the form field
+        let formText = document.getElementById('name').value
 
-}
-
-export function validURL(str) {
-  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-  return !!pattern.test(str);
+        console.log("::: Form Submitted :::")
+        fetch(baseURL+formText+apiKey)
+        .then(res => res.json())
+        .then(function(res) {
+            document.getElementById('results').innerHTML = res.city.name;
+            console.log(res);
+        });
 }
